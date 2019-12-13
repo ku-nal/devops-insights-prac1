5f79b0c77263a485769306fe07da6937fa3b3440
Shivam Solanki <Shivam.Raj.Solanki@ibm.com>
2019-12-13 11:03:41 -0600

    Adding all edited files

---
+++ build/precache-manifest.a76d7e7cbe2df255aaf91dce3d3dd739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5983cc277428fd5f2ba3e06840068981",
+    "url": "/index.html"
+  },
+  {
+    "revision": "005a48addf1192562c1a",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "df4314c9c1501b6914c2",
+    "url": "/static/css/main.4d068b18.chunk.css"
+  },
+  {
+    "revision": "005a48addf1192562c1a",
+    "url": "/static/js/2.198960a5.chunk.js"
+  },
+  {
+    "revision": "df4314c9c1501b6914c2",
+    "url": "/static/js/main.a480ea85.chunk.js"
+  },
+  {
+    "revision": "ee5dd88adc6ee133b458",
+    "url": "/static/js/runtime-main.bad403e0.js"
+  }
+]);
