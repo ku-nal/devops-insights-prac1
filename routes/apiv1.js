9f83f7feb18223602ca4da4bb82240b153b0a76f
Umesh Parulekar <ucparule@us.ibm.com>
2019-12-17 10:55:42 -0600

    added code to apiv1.js to manage the code coverage

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -39,6 +39,7 @@ exports.getWeather = function(req, res) {
 };
 router.get('/getWeather', exports.getWeather);
 
+/*
 exports.getWeather2 = function(req, res) {
 	var zip = req.query.zip;
 	if( (zip === null) || (typeof(zip) === 'undefined') ) {
@@ -68,5 +69,35 @@ exports.getWeather2 = function(req, res) {
 
 };
 router.get('/getWeather2', exports.getWeather2);
+*/
+exports.getWeather3 = function(req, res) {
+	var zip = req.query.zip;
+	if( (zip === null) || (typeof(zip) === 'undefined') ) {
+		return res.status(400).send('zip missing');
+	}
+
+	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+
+	request({
+		method: 'GET',
+        url: aurl,
+  		json: true
+    }, function(err, resp, body) {
+    	if(err) {
+    		res.status(400).send('Failed to get the data');
+    		//console.error("Failed to send request to openweathermap.org", err);
+    	} else {
+    		if(body.cod === 200) {
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+    			var response = {city: body.name, weather: weath};
+    			return res.status(200).send(response);
+    		} else {
+                return res.status(400).send({msg:'Failed'});
+            }
+    	}
+    });
+
+};
+router.get('/getWeather3', exports.getWeather3);
 
 exports.router = router;
