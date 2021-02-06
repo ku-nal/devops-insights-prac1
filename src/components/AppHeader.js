dad9cb95c5546ea6ce523b69ab69fc7fe9b84f02
Kunal Makwana <kunalmakwana18@gnu.ac.in>
2021-02-06 17:23:35 +0000

    Second Commit

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AppHeader() {
 
     return <div>
-        <p className="p-3 text-light h3 bg-primary">Current weather by zip codee</p>
+        <p className="p-3 text-light h3 bg-primary">Current weather by zip code Updated</p>
     </div>
 }
   
